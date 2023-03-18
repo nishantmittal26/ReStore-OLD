7044a9a8b496dce72545786add73af1d86efafe6
Nishant Mittal <nishantmittal26@outlook.com>
2023-03-18 21:47:48 +0530

    section 3- video 27

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,12 @@
+import Catalog from "../../features/catalog/catalog";
+
+function App() {
+  return (
+    <div>
+       <h1>Re-Store</h1>
+       <Catalog />
+    </div>
+  );
+}
+
+export default App;
